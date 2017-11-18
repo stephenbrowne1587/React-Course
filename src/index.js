edaaf4d837458298bd28c398a7bc64df05298709
Stephen Browne <stephenbrowne1587@gmail.com>
2017-11-18 10:48:47 -0500

    improved search bar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
+import _ from "lodash";
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 import SearchBar from "./Components/search_bar";
+import VideoList from "./Components/video_list";
+import VideoDetail from "./Components/video_detail";
 
 const API_KEY = "AIzaSyD05vrSVVzlt_Mtl1jHsXd5rCgX0viytAg";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+     };
+    this.videoSearch("pure desmond")
+  }
+
+  videoSearch(term) {
+    YTSearch( {key: API_KEY, term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} 
+        />
+        
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
